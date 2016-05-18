47166883b0f229b99f25908307712a38a2a42974
elChifo <antonioperez.acn@gmail.com>
2016-05-18 21:56:38 +0200

    editar OK. sin fallos

---
+++ application/view/plates/usuarios/noBorrado.php
@@ -0,0 +1,10 @@
+<?php $this->layout('layout') ?> 
+
+<div class="container">    
+
+    <h2>El Usuario está inscrito en un partido y no puede ser eliminado </h2> 
+	<p>
+    <a href="../usuarios/administrar"><h2>Volver</h2></a>
+	</p>
+
+</div>
