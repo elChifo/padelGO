110801962d7ea29efedab48b2c9ab7b7a31d3af6
Alberto Padilla <albepadilla@hotmail.com>
2016-05-21 18:12:45 +0200

    nuevos cambios

--- application/view/plates/layout.php
+++ application/view/plates/layout.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title><?= $titulo ?></title>
     <meta name="description" content="">
+    <link rel="shortcut icon" href="<?php echo URL; ?>img/padelgoico.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- JS -->
@@ -16,41 +17,71 @@
     </script>
 
     <!-- our JavaScript -->
-        
-    <!-- BOOTSTRAP 
-        <script type="text/javascript" src="<?php echo URL; ?>js/bootstrap.min.js"></script>
-    -->
-    <script type="text/javascript" src="<?php echo URL; ?>js/codigoform.js"></script>   
+
+    <!-- BOOTSTRAP -->
+    <script type="text/javascript" src="<?php echo URL; ?>js/bootstrap.min.js"></script>
+
     <script type="text/javascript" src="<?php echo URL; ?>js/jcalendar/calendar.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>js/jcalendar/calendar-es.js"></script>
     <script type="text/javascript" src="<?php echo URL; ?>js/jcalendar/calendar-setup.js"></script>
         
     <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/bootstrap.min.css">
 
-    <!-- BOOTSTRAP 
-        <link rel="stylesheet" type="text/css" 
-                href="<?php echo URL; ?>css/bootstrap.css"> 
-
-        <link rel="stylesheet" type="text/css" 
-                href="<?php echo URL; ?>css/bootstrap.min.css"> 
-    -->
-
-    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/style.css">
-    <link type="text/css" rel="stylesheet" href="<?php echo URL; ?>css/styleform.css">
+    <!--<link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/style.css">-->
+    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/estilo.css">
     <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/jcalendar/theme.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/font-awesome.css">
 </head>
-<body style="background-color: rgba(170, 250, 5, 0.4); ">
-    <!-- logo -->
-    <div class="logo">Padel GO!
-    </div>
+<body>
     <!-- navigation -->
-<?php $this->insert('partials/menu') ?> 
-
-<?= $this->section('content') ?>
+    <div class="col-md-12 cabecera">
+        <!-- logo -->
+        <div class="col-md-2 logo">
+             <a title="Inicio" href="<?php echo URL; ?>"><img src="<?php echo URL; ?>img/home.png" alt="Padel GO!" height="100"></a>
+        </div>
+        <div class="col-md-8 menu">
+            <?php $this->insert('partials/menu') ?> 
+        </div>
+        <div class="col-md-2 zona-login">
+            <?php if (isset($_SESSION['idUsuario'])) : ?> 
+                <p>Estas logueado como: <b class="pColor"><?= Session::get('nombre') ?></b></p>
+                <a class="enlaceLogin" title="Mi cuenta" href="<?php echo URL; ?>login">Mi cuenta <img src="<?php echo URL; ?>img/micuenta.png" alt="Mi cuenta" height="15" /></a><br>
+                <button type="button" class="botonlogin" title="Cerrar Sesión" onclick="window.location.href='<?php echo URL; ?>login/salir'">Cerrar Sesión</button>
+            <?php else : ?>
+                <button type="button" class="botonlogin" title="Registrarse" onclick="window.location.href='<?php echo URL; ?>registro'">Registrarse</button>
+                <button type="button" class="botonlogin" title="Iniciar Sesión" onclick="window.location.href='<?php echo URL; ?>login'">Iniciar Sesión</button>
+            <?php endif ?>
+        </div>
+    </div>
+    <?= $this->section('content') ?>
+    <!-- footer -->
+    <div class="col-md-12 pie">
+        <div class="col-md-3 divpie">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin vestibulum purus, sed viverra purus tristique suscipit. Quisque ullamcorper nunc congue mi porttitor posuere. Nam sem lectus, commodo elementum porta non, suscipit ac dui. Donec pellentesque felis magna, vel varius purus venenatis ac. Phasellus eu diam vel elit gravida fringilla a sed sem. Duis aliquet a nibh non vehicula. Nullam fringilla euismod justo, vel interdum quam elementum ac. 
+        </div>
+        <div class="col-md-3 divpie">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin vestibulum purus, sed viverra purus tristique suscipit. Quisque ullamcorper nunc congue mi porttitor posuere. Nam sem lectus, commodo elementum porta non, suscipit ac dui. Donec pellentesque felis magna, vel varius purus venenatis ac. Phasellus eu diam vel elit gravida fringilla a sed sem. Duis aliquet a nibh non vehicula. Nullam fringilla euismod justo, vel interdum quam elementum ac. 
+        </div>
+        <div class="col-md-3 divpie">
+        <div id="cont_5646df3e0bb3848a035b26e2dfbe669d"><span id="h_5646df3e0bb3848a035b26e2dfbe669d"><a id="a_5646df3e0bb3848a035b26e2dfbe669d" href="http://www.tiempo.com/murcia.htm" target="_blank" style="color: #00ABEB; font-family: 1; font-size: 13px; text-decoration: none;">El tiempo en MURCIA</a></span><script type="text/javascript" async src="https://www.tiempo.com/wid_loader/5646df3e0bb3848a035b26e2dfbe669d"></script></div>
+        </div>
+        <div class="col-md-3 divpie">
+            <a href="#" target="_blank"><img alt="Siguenos en facebook" height="48" src="<?php echo URL; ?>img/facebook.png" title="Siguenos en facebook" width="48" /></a>
+            <a href="#" target="_blank"><img alt="Siguenos en Twitter" height="48" src="<?php echo URL; ?>img/twitter.png" title="Siguenos en Twitter" width="48" /></a>
+            <a href="#" target="_blank"><img alt="Canal de youtube" height="48" src="<?php echo URL; ?>img/youtube.png" title="Canal de youtube" width="48" /></a> 
+            <a href="#" target="_blank"><img alt="Rss feed" height="48" src="<?php echo URL; ?>img/rss.png" title="Rss feed" width="48" /></a>
+            <a href="#" target="_blank"><img alt="Siguenos en Instagram" height="48" src="<?php echo URL; ?>img/instagram.png" title="Siguenos en Instagram" width="48" /></a>
+            <a href="<?php echo URL; ?>contacto" target="_blank"><img alt="Envíanos un Correo" height="48" src="<?php echo URL; ?>img/correo.png" title="Envíanos un Correo" width="48" /></a>
+        </div>
+    </div>
 
     <!-- jQuery, loaded in the recommended protocol-less way -->
     <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
     
 </body>
 </html>
+
 
