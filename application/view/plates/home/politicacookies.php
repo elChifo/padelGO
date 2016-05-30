210bc73c71e82fd7d19de0b744a4fe9cefeff386
jmmf89 <jmmirete89@gmail.com>
2016-05-31 00:42:17 +0200

    vista politica cookies completa

---
+++ application/view/plates/home/politicacookies.php
@@ -0,0 +1,43 @@
+<?php $this->layout('layout') ?>
+<div class="container">
+	<div class="col-md-12">
+		<div class="col-md-4">
+		
+		<h3> POLÍTICA DE COOKIES </h3>
+		    <p><?= $presentacion ?></p>
+		</div>
+		
+		<div class="col-md-4">	
+			<h3> ¿Qué son las Cookies?  </h3>    
+		    <p> -Una cookie es un fichero que se descarga en su ordenador al acceder a 
+				determinadas páginas web. Las cookies permiten a una página web, entre otras 
+				cosas, almacenar y recuperar información sobre los hábitos de navegación de un
+				usuario o de su equipo y, dependiendo de la información que contengan y de la
+				forma en que utilice su equipo, pueden utilizarse para reconocer al usuario. 
+		    </p>
+		    <h3> Cookies de analisis estadístico </h3>  
+		    <p> 
+		     -Son aquellas que, bien tratadas por nosotros o por terceros, permiten 
+			cuantificar el número de visitantes y analizar estadísticamente la utilización
+			que hacen los usuarios de nuestros servicios. Gracias a ellas podemos estudiar
+			la navegación por nuestra página web y mejorar así la oferta de productos o 
+			servicios que ofrecemos. Estas cookies no irán asociadas a ningún dato de 
+			carácter personal que pueda identificar al usuario, dando información sobre el 
+			comportamiento de navegación de forma anónima.
+			  </p>
+		    <h3> Cookies técnicas </h3>  
+		    <p> 
+		     -Son aquéllas que permiten al usuario la navegación a través de una página 
+				web, plataforma o aplicación y la utilización de las diferentes opciones 
+				servicios que en ella existan como, por ejemplo, controlar el tráfico y la 
+				comunicación de datos, identificar la sesión, acceder a partes de acceso 
+				restringido, recordar los elementos que integran un pedido, realizar el proceso
+				de compra de un pedido, realizar la solicitud de inscripción o participación en
+				un evento, utilizar elementos de seguridad durante la navegación, almacenar 
+				contenidos para la difusión de videos o sonido o compartir contenidos a través 
+				de redes sociales
+			  </p>
+		</div>
+	</div>
+</div>
+
