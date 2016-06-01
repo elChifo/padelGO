83abafa421fd725eafcf3f8dc3cf63983636e56a
Alberto Padilla <albepadilla@hotmail.com>
2016-06-02 00:27:58 +0200

    modificado tamaño botones menu

--- application/view/plates/partials/menu.php
+++ application/view/plates/partials/menu.php
@@ -1,8 +1,8 @@
 <div class="navigation">
-    <a title="INICIO" href="<?php echo URL; ?>"><img src="<?php echo URL; ?>img/menu/inicio.png" alt="INICIO" height="85" class="imagen-menu1" /></a>
-    <a title="PARTIDOS" href="<?php echo URL; ?>partidos"><img src="<?php echo URL; ?>img/menu/partidos.png" alt="PARTIDOS" height="85" class="imagen-menu2" /></a>
-    <a title="NOTICIAS" href="<?php echo URL; ?>noticias"><img src="<?php echo URL; ?>img/menu/noticias.png" alt="NOTICIAS" height="85" class="imagen-menu3" /></a>
-    <a title="USUARIOS" href="<?php echo URL; ?>usuarios"><img src="<?php echo URL; ?>img/menu/usuarios.png" alt="USUARIOS" height="85" class="imagen-menu4" /></a>
-    <a title="CLUBS" href="<?php echo URL; ?>clubs"><img src="<?php echo URL; ?>img/menu/clubs.png" alt="CLUBS" height="85" class="imagen-menu5" /></a>
-    <a title="CONTACTO" href="<?php echo URL; ?>contacto"><img src="<?php echo URL; ?>img/menu/contacto.png" alt="CONTACTO" height="85" class="imagen-menu6" /></a>
+    <a title="INICIO" href="<?php echo URL; ?>"><img src="<?php echo URL; ?>img/menu/inicio.png" alt="INICIO" height="80" class="imagen-menu1" /></a>
+    <a title="PARTIDOS" href="<?php echo URL; ?>partidos"><img src="<?php echo URL; ?>img/menu/partidos.png" alt="PARTIDOS" height="80" class="imagen-menu2" /></a>
+    <a title="NOTICIAS" href="<?php echo URL; ?>noticias"><img src="<?php echo URL; ?>img/menu/noticias.png" alt="NOTICIAS" height="80" class="imagen-menu3" /></a>
+    <a title="USUARIOS" href="<?php echo URL; ?>usuarios"><img src="<?php echo URL; ?>img/menu/usuarios.png" alt="USUARIOS" height="80" class="imagen-menu4" /></a>
+    <a title="CLUBS" href="<?php echo URL; ?>clubs"><img src="<?php echo URL; ?>img/menu/clubs.png" alt="CLUBS" height="80" class="imagen-menu5" /></a>
+    <a title="CONTACTO" href="<?php echo URL; ?>contacto"><img src="<?php echo URL; ?>img/menu/contacto.png" alt="CONTACTO" height="80" class="imagen-menu6" /></a>
 </div>
