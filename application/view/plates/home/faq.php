607016d2ade2bb1941bdc072fa7ea993154d068e
jmmf89 <jmmirete89@gmail.com>
2016-06-02 23:22:59 +0200

    faq

---
+++ application/view/plates/home/faq.php
@@ -0,0 +1,30 @@
+<?php $this->layout('layout') ?>
+<div class="container">
+	<div class="col-md-12">
+		<div class="col-md-4">
+
+			<script>!window.jQuery && document.write(unescape('%3Cscript src="<?= URL; ?>jsjquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
+
+			<input type="button" value="faq" class="button" data-type="zoom" />
+			
+			<div class="overlay-container">
+				<div class="ventana zoom">
+
+					<h3>FAQ</h3> 
+					<h2>Preguntas Frecuentes </h2>
+
+					A los efectos de lo previsto en la Ley Orgánica 15/99 de 13 de Diciembre de Protección de datos de carácter personal, PadelGo informa al usuario de la existencia de un fichero automatizado de datos personales creado por y para PadelGo, y bajo su responsabilidad, con la finalidad de realizar el mantenimiento y gestión de la relación con el usuario, así como las labores de información, formación y comercialización del servicio y de las actividades relacionadas con los mismos.<br/>
+					<br/>
+					En el momento de aceptación de las presentes Condiciones Generales, PadelGo, precisará de la recogida de unos datos básicos e imprescindibles para la prestación de los servicios. Dichos datos son: Nombre, apellidos, dirección, código Postal, y teléfono del Usuario. En el momento de la aceptación del presente contrato el Usuario será informado del carácter necesario de la recogida de tales datos
+					<br/>
+					El usuario presta su consentimiento para que PadelGo, pueda hacer uso de sus datos de navegación por Internet a fin de remitirle publicidad de terceros acorde con los sitios web que visite. Los anunciantes serán los únicos responsables de la veracidad, exactitud y legalidad de su publicidad siendo PadelGo un mero difusor de la misma.<br/>
+					<br/>			
+					De conformidad con lo establecido en las condiciones generales el usuario conoce y acepta el empleo de la tecnología "Cookie" por parte de PadelGo, si bien puede configurar su navegador para ser avisado de ello e impedir la instalación de "Cookies" en su disco duro, por favor, consulte instrucciones y manual de su navegador para completar esta información.
+					<span class="close">Cerrar</span>
+				</div>		
+			</div>
+
+		</div>
+	</div>
+</div>
+
