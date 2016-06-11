2d5469d2feffc2d69a155cd89d7cc432e6caf78e
elChifo <antonioperez.acn@gmail.com>
2016-06-11 15:43:45 +0200

    Favoritos OK

--- application/model/favoritosmodel.php
+++ application/model/favoritosmodel.php
@@ -18,11 +18,98 @@ class FavoritosModel
         if($id == 0){
             return false;
         }
-        $ssql = "SELECT * FROM Favoritos WHERE idFavorito = :idFavorito";
+        $ssql = "SELECT * FROM Favoritos WHERE idUsuario = :idUsuario";
         $query = $conn->prepare($ssql);
-        $query->bindValue(":idFavorito", $id, PDO::PARAM_INT);
+        $query->bindValue(":idUsuario", $id, PDO::PARAM_INT);
         $query->execute();
         return $query->fetch();
 
     }
+
+    public static function existeFavorito($datos) {
+        $conn = Database::getInstance()->getDatabase();      
+
+        $params = array(
+            'idUsuario' => $datos['idUsuario'],
+            'Favorito' => $datos['Favorito']
+        );
+
+        $ssql = "SELECT * FROM Favoritos WHERE Favorito = :Favorito AND idUsuario = :idUsuario";
+        
+        $query = $conn->prepare($ssql);
+        $query->execute($params);
+        $cuenta = $query->rowCount();
+        var_dump($cuenta);
+
+        if($cuenta == 1) {
+            return false;
+        }
+        else{            
+            return true;
+        }        
+    }
+
+    public static function agregar($favorito)  {
+        $conn = Database::getInstance()->getDatabase();
+        
+        $params = array(
+            'idUsuario' => $favorito['idUsuario'],
+            'Favorito' => $favorito['Favorito']
+        );
+
+        $ssql = "INSERT INTO Favoritos (idUsuario, Favorito)
+                VALUES (:idUsuario, :Favorito)";
+
+        $query = $conn->prepare($ssql);
+        $query->execute($params);
+        $cuenta = $query->rowCount();
+
+        if($cuenta == 1) {
+            return $conn->lastInsertId();
+        }
+        return false;
+        
+    }
+
+     public static function quitar($favorito)  {
+        $conn = Database::getInstance()->getDatabase();
+
+        $params = array(
+            ":idUsuario" => $favorito["idUsuario"],
+            ":Favorito" => $favorito["Favorito"]
+        );
+
+        $ssql = "DELETE FROM Favoritos WHERE Favorito=:Favorito AND idUsuario=:idUsuario";
+
+        $query = $conn->prepare($ssql);
+        $query->execute($params);  
+        $cuenta = $query->rowCount();
+
+        if($cuenta == 1) {
+            return $conn->lastInsertId();
+        }
+        return false;
+               
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- application/view/plates/usuarios/index.php
+++ application/view/plates/usuarios/index.php
@@ -1,7 +1,8 @@
 <?php $this->layout('layout') ?>
-  <?php $idSession = Session::get('idUsuario'); ?>
+  <?php $idSession = Session::get('idUsuario');  ?> 
 
 <div class="container">
+    <?php $this->insert('partials/feedback') ?>
        
     <?php if(count($usuarios) == 0): ?>
 
@@ -9,48 +10,35 @@
 
     <?php else: ?>
         
-        <h3 class="titulo-pagina">USUARIOS</h3> 
+        <h3 class="titulo-pagina">USUARIOS</h3>
 
-         <?php foreach($usuarios as $usuario): ?>
+         <?php foreach($usuarios as $usuario): ?>          
            
-           <?php if ( ($usuario->idUsuario != $idSession) && ($usuario->idUsuario != 1) ): ?>
+           <?php if (($usuario->idUsuario != $idSession) && ($usuario->idUsuario != 1)) : ?>
 
-              <fieldset style="border: 2px solid navy; width: 100rem; padding: 1rem;"> 
+            <fieldset style="border: 2px solid navy; width: 100rem; padding: 1rem;"> 
               <table border="1" class="tablapartidos">
              <tr><td>
-              <ul> 
-
-              <?php foreach ($favoritos as $favorito): ?>  
-                
-                    <?php if ($usuario->idUsuario == $favorito->Favorito): ?>
-
-                      <?php if ($usuario->idUsuario != $favorito->idUsuario): ?>  
-
-
-                      <a href="../favoritos/quitar?Favorito=<?= $usuario->idUsuario; ?>">
-                        <img src="<?= URL; ?>img/favorito.png" alt="FAVORITO" height="30" /></a>
-                    
-                      <?php endif ?>
-
-
-                    <?php else: ?>
-
-
-
+              <ul>
               
-                 <a href="../favoritos/agregar?Favorito=<?= $usuario->idUsuario; ?>">
-                    <img src="<?= URL; ?>img/favoritoVacio.png" alt="FAVORITO VACIO" height="30" /></a>
+              <?php foreach ($favoritos as $favorito): ?> 
+                                    
+                 <?php if ($idSession == $favorito->idUsuario) :?>
 
+                  <?php if ($usuario->idUsuario == $favorito->Favorito): ?>                                   
+                    <a href="../favoritos/quitar?idUsuario=<?= $usuario->idUsuario; ?>" 
+                        style="float:right;">
+                    <img src="<?= URL; ?>img/favorito.png" alt="FAVORITO" height="50" /></a>
+          
+                  <?php endif ?>                  
 
-
-
-               <?php endif ?> 
-
-              <?php endforeach ?>               
+                <?php endif ?>
+              
+              <?php endforeach ?>
 
                 <li> NOMBRE: 
                     <strong> 
-                        <?= $usuario->nombre . ' ' . $usuario->apellidos; ?> 
+                        <?= $usuario->nombre . ' ' . $usuario->apellidos; ?>
                     </strong>                                      
                 </li>
                 <br>
@@ -67,6 +55,12 @@
             </div>
             </td></tr>
             </table>
+
+            <?php if (isset($idSession)): ?>                 
+                  <a href="../favoritos/agregar?idUsuario=<?= $usuario->idUsuario; ?>">Agregar a Favoritos
+                </a>
+              <?php endif ?>    
+           
            </fieldset>
 
            <?php endif ?>
