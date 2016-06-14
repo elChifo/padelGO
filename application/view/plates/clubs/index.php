a7d1f822cab3c4dd7564130b3016409961a0b4ee
Alberto Padilla <albepadilla@hotmail.com>
2016-06-14 23:12:31 +0200

    Add files via upload

--- application/view/plates/clubs/index.php
+++ application/view/plates/clubs/index.php
@@ -1,8 +1,7 @@
 <?php $this->layout('layout') ?>
   <?php $idSession = Session::get('idUsuario'); ?>
 
-<div class="container">
-    <?php $this->insert('partials/feedback') ?>
+<div class="container" style="margin-left: -30px;padding-left: 3%; padding-right: 3%;">
 
 	<?php if(count($clubs) == 0): ?>
 
@@ -13,19 +12,12 @@
         <h3 class="titulo-pagina">CLUBS</h3>  
 
 		<?php foreach($clubs as $club): ?>
-
-		   <div class="divClub" style="width: 30%;float:left;"> Nombre Club:
-					   			<strong> 
-					   				<?= $club->nombreClub ?> &nbsp;&nbsp;&nbsp; 
-					   				
-					   			</strong> 			
-				   					<p> Direccion : <?= $club->direccionClub ?>  </p>
-				   					<p> Nº Pistas: <?= $club->numPistas ?>  </p>	
-						<a href="../partidos/ver?idClub=<?= $club->idClub; ?>">Ver Partidos</a>
-				<div class="imagenClub">
-	                <img src="<?= URL; ?>img/clubs/club<?= $club->idClub; ?>.png" 
-	                title="Club<?= $club->nombreClub; ?>" height="150" width="325" /> 
-	            </div>
+		    <div class="divClub" style="width: 30%;float:left;"> 
+		    	<h4><b><?= $club->nombreClub ?> &nbsp;&nbsp;&nbsp;</b></h4>	
+	            <img src="<?= URL; ?>img/clubs/club<?= $club->idClub; ?>.png" title="Club<?= $club->nombreClub; ?>" height="125" width="275" style="border: 1px solid #333"/> <br><br>		
+				<p> <b>Direccion:</b> <?= $club->direccionClub ?>  </p>
+				<p> Nº Pistas: <b><?= $club->numPistas ?></b>  </p>	
+				<a class="colorPadelGO2" href="../partidos/ver?idClub=<?= $club->idClub; ?>">Ver Partidos</a>
 			</div>
 
 		<?php endforeach ?>
