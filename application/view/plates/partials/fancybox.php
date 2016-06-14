86be9e09c5c845091b66c992b5e1912bdf6de6cf
elChifo <antonioperez.acn@gmail.com>
2016-06-14 20:00:50 +0200

    Nuevos cambios + intento Fancybox

--- application/view/plates/partials/fancybox.php
+++ application/view/plates/partials/fancybox.php
@@ -5,5 +5,26 @@
         <a href="#close" title="Cerrar" onclick="toogle('none','ventana_aviso','aviso')"/>
     </div>    
 </div>
+<div id="ventana_cookies" style="display: none;">
+    <div id="cookies" class="contenedor" style="display: none;">       
+        <iframe name="principal" width="100%" height="600" src="<?= URL; ?>text/cookies.html">
+        </iframe>
+        <a href="#close" title="Cerrar" onclick="toogle('none','ventana_cookies','cookies')"/>
+    </div>    
+</div>
+<div id="ventana_padelgo" style="display: none;">
+    <div id="padelgo" class="contenedor" style="display: none;">       
+        <iframe name="principal" width="100%" height="600" src="<?= URL; ?>text/padelgo.html">
+        </iframe>
+        <a href="#close" title="Cerrar" onclick="toogle('none','ventana_padelgo','padelgo')"/>
+    </div>    
+</div>
+<div id="ventana_faq" style="display: none;">
+    <div id="faq" class="contenedor" style="display: none;">       
+        <iframe name="principal" width="100%" height="600" src="<?= URL; ?>text/faq.html">
+        </iframe>
+        <a href="#close" title="Cerrar" onclick="toogle('none','ventana_faq','faq')"/>
+    </div>    
+</div>
 
 
