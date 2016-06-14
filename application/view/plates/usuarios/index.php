ee6dbc05fbec783efa2891590ee75d527b32bed2
Alberto Padilla <albepadilla@hotmail.com>
2016-06-14 20:41:47 +0200

    Add files via upload

--- application/view/plates/usuarios/index.php
+++ application/view/plates/usuarios/index.php
@@ -1,9 +1,9 @@
 <?php $this->layout('layout') ?>
   <?php $idSession = Session::get('idUsuario');  ?> 
 
-<div class="container">
+<div class="container" style="margin-left: -30px;padding-left: 3%; padding-right: 3%;">
     <?php $this->insert('partials/feedback') ?>
-       
+       <div class="col-md-12">
     <?php if(count($usuarios) == 0): ?>
 
         <h3 class="titulo-pagina">No se encuentran Usuarios en la Base de Datos</h3>
@@ -11,62 +11,54 @@
     <?php else: ?>
         
         <h3 class="titulo-pagina">USUARIOS</h3>
-
+        
          <?php foreach($usuarios as $usuario): ?>          
            
            <?php if (($usuario->idUsuario != $idSession) && ($usuario->idUsuario != 1)) : ?>
-
-            <fieldset style="border: 2px solid navy; width: 100rem; padding: 1rem;"> 
-              <table border="1" class="tablapartidos">
-             <tr><td>
-              <ul>
+                <?php if ($usuario->idCategoria == '1') : ?>
+                      <div class="divUsuario1" style="width: 30%;float:left;">
+                <?php elseif ($usuario->idCategoria == '2') : ?>
+                      <div class="divUsuario2" style="width: 30%;float:left;">
+                <?php elseif ($usuario->idCategoria == '3') : ?>
+                      <div class="divUsuario3" style="width: 30%;float:left;">
+                <?php elseif ($usuario->idCategoria == '4') : ?>
+                      <div class="divUsuario4" style="width: 30%;float:left;">
+                <?php else : ?>
+                      <div class="divUsuario5" style="width: 30%;float:left;">
+                <?php endif ?>
               
-              <?php foreach ($favoritos as $favorito): ?> 
+                  <div class="imagenUsuario">
+                    <img src="<?= URL; ?>img/usuarios/usuario<?= $usuario->idUsuario; ?>.png" 
+                    height="100" width="100" alt="usuario<?= $usuario->idUsuario; ?>" class="usuarioRedonda"/> 
+                  </div>
+                  
+                  <font style="color:#FFF; font-size: 150%;"><b><?= $usuario->nombre . ' ' . $usuario->apellidos; ?></b></font><br><br>
+                  <font style="color:#FFF; font-size: 150%;"><b><?= $usuario->idCategoria ?>ª Categoría<a href=""></a></b></font><br>
+                  <?php foreach ($favoritos as $favorito): ?> 
                                     
                  <?php if ($idSession == $favorito->idUsuario) :?>
 
                   <?php if ($usuario->idUsuario == $favorito->Favorito): ?>                                   
                     <a href="../favoritos/quitar?idUsuario=<?= $usuario->idUsuario; ?>" 
-                        style="float:right;">
-                    <img src="<?= URL; ?>img/favorito.png" alt="FAVORITO" height="50" /></a>
-          
+                        style="float:right;position: absolute;">
+                    <img src="<?= URL; ?>img/favorito.png" title="Borrar de mis Favoritos" alt="FAVORITO" height="50" /></a>
+                  <?php else : ?>
+
                   <?php endif ?>                  
 
                 <?php endif ?>
               
               <?php endforeach ?>
-
-                <li> NOMBRE: 
-                    <strong> 
-                        <?= $usuario->nombre . ' ' . $usuario->apellidos; ?>
-                    </strong>                                      
-                </li>
-                <br>
-                <li> CATEGORÍA: 
-                    <strong> 
-                        <?= $usuario->idCategoria . 'ª Categoría'; ?> 
-                    </strong>                                      
-                </li>
-           </ul>
-
-           <div class="imagenUsuario">
-                <img src="<?= URL; ?>img/usuarios/usuario<?= $usuario->idUsuario; ?>.png" 
-                alt="usuario<?= $usuario->idUsuario; ?>" height="100" style="border-radius: 100px;"/> 
-            </div>
-            </td></tr>
-            </table>
-
-            <?php if (isset($idSession)): ?>                 
-                  <a href="../favoritos/agregar?idUsuario=<?= $usuario->idUsuario; ?>">Agregar a Favoritos
-                </a>
-              <?php endif ?>    
-           
-           </fieldset>
+                  <?php if (isset($idSession)): ?>      
+                      <a href="../favoritos/agregar?idUsuario=<?= $usuario->idUsuario; ?>" title="Agregar a mis Favoritos"><img src="<?= URL; ?>img/favoritoVacio.png" title="Agregar a mis Favoritos" alt="FAVORITO" height="50" /></a>
+                  <?php endif ?> 
+                </div>
 
            <?php endif ?>
 
         <?php endforeach ?> 
-
+        
     <?php endif ?>
+    </div>
     <a href='#' onclick='subir();return false' title='Ir Arriba' class="flecha"><img src="<?= URL; ?>img/flecha.png"></a>
 </div>
